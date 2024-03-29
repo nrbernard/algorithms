83aa55daf623d5c9b39d0929078be43f48aa369e
Nick Bernard <nick.bernard@opensesame.com>
2024-03-29 16:18:05 -0700

    Dynamically import functions in tests to reuse test file for different implementations

---
+++ src/sorts/bubble/bubble.test.ts
@@ -0,0 +1,37 @@
+import { beforeAll, describe, expect, test } from 'vitest';
+
+const names = ['nick'];
+const functionName = 'bubbleSort';
+
+names.forEach((name) => {
+  let sortFunction: (arr: number[]) => number[];
+  const fileName = `./${name}-${functionName}.ts`;
+
+  beforeAll(async () => {
+    const module = await import(fileName);
+
+    sortFunction = module[functionName];
+  });
+
+  describe(name, () => {
+    test('it sorts the array when the input array is already sorted', () => {
+      expect(sortFunction([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+    });
+
+    test('it sorts the array when the input array is sorted in descending order', () => {
+      expect(sortFunction([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
+    });
+
+    test('it sorts the array when the input array has duplicate values', () => {
+      expect(sortFunction([5, 3, 4, 5, 3])).toEqual([3, 3, 4, 5, 5]);
+    });
+
+    test('it sorts the array when the input array has one element', () => {
+      expect(sortFunction([5])).toEqual([5]);
+    });
+
+    test('it sorts the array when the input array is empty', () => {
+      expect(sortFunction([])).toEqual([]);
+    });
+  });
+});
