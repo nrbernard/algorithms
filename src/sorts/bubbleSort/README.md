970e2ec56a1d2d1e9f30b0766e00d0786810696f
Nick Bernard <nick.bernard@opensesame.com>
2024-04-03 15:58:25 -0700

    Add bubbleSort implementations and README

---
+++ src/sorts/bubbleSort/README.md
@@ -0,0 +1,13 @@
+# Bubble Sort
+
+The bubble sort algorithm steps through the list to be sorted, compares each pair of adjacent elements, and swaps them if they are out of order. It repeats the pass through the list until no swaps are needed and the list is sorted.
+
+Smaller values "bubble" up to the top of the list while larger, "heavier" numbers "sink" to the bottom.
+
+The worst-case and average complexity is O(n^2).
+
+## See Also
+
+- https://xlinux.nist.gov/dads/HTML/bubblesort.html
+- https://www.toptal.com/developers/sorting-algorithms/bubble-sort
+- https://youtu.be/6Gv8vg0kcHc
--- src/sorts/bubbleSort/nick-bubbleSort.ts
+++ src/sorts/bubbleSort/nick-bubbleSort.ts
@@ -1,5 +1,42 @@
 export function bubbleSort(array: number[]): number[] {
-  // This is a placeholder for the bubble sort implementation
+  const n = array.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let swapped = false;
+
+    for (let j = 0; j < n - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+
+        swapped = true;
+      }
+    }
+
+    if (!swapped) {
+      break;
+    }
+  }
 
   return array;
+}
+
+export function bubbleSortRecursive(
+  array: number[],
+  n = array.length
+): number[] {
+  if (n <= 1) {
+    return array;
+  }
+
+  for (let i = 0; i < n - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      const temp = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = temp;
+    }
+  }
+
+  return bubbleSortRecursive(array, n - 1);
 }
