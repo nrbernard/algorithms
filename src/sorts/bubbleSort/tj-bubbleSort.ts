83625395e3f8d1bb343281ff687ce8beab8960b2
TJ Rivas <tj.rivas@opensesame.com>
2024-04-04 10:36:31 -0700

    TJs bubblesort implementation

--- src/sorts/bubbleSort/tj-bubbleSort.ts
+++ src/sorts/bubbleSort/tj-bubbleSort.ts
@@ -1,5 +1,16 @@
 export function bubbleSort(array: number[]): number[] {
   // This is a placeholder for the bubble sort implementation
+  const outputArray = [...array];
 
-  return array;
+  for(let i = 0; i < outputArray.length; i++) {
+    for(let j = 0; j < outputArray.length - i - 1; j++) {
+      if(outputArray[j] > outputArray[j + 1]) {
+        const temp = outputArray[j];
+        outputArray[j] = outputArray[j + 1];
+        outputArray[j + 1] = temp;
+      }
+    }
+  }
+
+  return outputArray;
 }
