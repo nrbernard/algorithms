b9bb3e9d47ddb9763ba428801b09f68376b61ce5
TJ Rivas <tj.rivas@opensesame.com>
2024-04-09 09:13:45 -0700

    TJ's quicksort

--- src/sorts/quicksort/tj-quickSort.ts
+++ src/sorts/quicksort/tj-quickSort.ts
@@ -1,3 +1,19 @@
 export function quickSort(array: number[]): number[] {
-  return [];
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const pivot = array[0];
+  const leftArray = [];
+  const rightArray = [];
+  
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      leftArray.push(array[i]);
+    } else {
+      rightArray.push(array[i]);
+    }
+  }
+
+  return quickSort(leftArray).concat(pivot, quickSort(rightArray));
 }
