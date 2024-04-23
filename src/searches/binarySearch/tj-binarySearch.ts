4c7d622996620fa1d6e1d545d488f620b0d3705c
TJ Rivas <tj.rivas@opensesame.com>
2024-04-23 15:03:45 -0700

    TJs binary search

--- src/searches/binarySearch/tj-binarySearch.ts
+++ src/searches/binarySearch/tj-binarySearch.ts
@@ -1,5 +1,23 @@
-export function binarySearch(array: number[]) {
-  return "TJ's binary search";
+export function binarySearch(array: number[]): number {
+  let low = 0;
+  let high = array.length - 1;
+  let mid = 0;
+  let guess = 0;
 
-  return array;
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2);
+    guess = array[mid];
+
+    if (guess === mid) {
+      return mid;
+    }
+
+    if (guess > mid) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
 }
